3e5444972f267cf9904989cec958d70bf2d5a756
igromanru <igromanru@yahoo.de>
2024-10-27 17:07:37 +0100

    feat: Add LoadAsset to ensure hooks success

--- Scripts/main.lua
+++ Scripts/main.lua
@@ -13,7 +13,7 @@ UnlockPermanently = false
 -- Don't change code below --
 ------------------------------
 ModName = "CustomizationUnlocker"
-ModVersion = "1.0.0"
+ModVersion = "1.0.1"
 
 local function ModInfoAsPrefix()
     return "["..ModName.." v"..ModVersion.."] "
@@ -21,17 +21,20 @@ end
 
 print(ModInfoAsPrefix().."Starting mod initialization\n")
 
-RegisterHook("/Game/Blueprints/Meta/Abiotic_GameInstance.Abiotic_GameInstance_C:IsCustomizationRowUnlocked", function(Context, RowName, Unlocked)
-    local gameInstance = Context:get() ---@type UAbiotic_GameInstance_C
-    local rowName = RowName:get()
-    local unlocked = Unlocked:get()
-
-    if not unlocked then
-        Unlocked:set(true)
-        if UnlockPermanently then
-            gameInstance:UnlockCustomization(rowName)
+ExecuteInGameThread(function()
+    LoadAsset("/Game/Blueprints/Meta/Abiotic_GameInstance.Abiotic_GameInstance_C")
+    RegisterHook("/Game/Blueprints/Meta/Abiotic_GameInstance.Abiotic_GameInstance_C:IsCustomizationRowUnlocked", function(Context, RowName, Unlocked)
+        local gameInstance = Context:get() ---@type UAbiotic_GameInstance_C
+        local rowName = RowName:get()
+        local unlocked = Unlocked:get()
+    
+        if not unlocked then
+            Unlocked:set(true)
+            if UnlockPermanently then
+                gameInstance:UnlockCustomization(rowName)
+            end
         end
-    end
+    end)
 end)
 
 print(ModInfoAsPrefix().."Mod loaded successfully\n")
